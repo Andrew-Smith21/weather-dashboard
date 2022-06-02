26f90f7d402f9d27ce0f4966ead0fb1af8d66974
Chad Smith <chad.smith@vanderbilt.edu>
2022-06-02 07:57:18 -0400

    added fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
+var city = "London";
+var cityUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&APPID=e53413e7d520a9be0760317a0280fb3f";
 
-
-var response = fetch(
-    "https://api.openweathermap.org/geo/1.0/direct?q=London&APPID=e53413e7d520a9be0760317a0280fb3f").then(
-        function(response) {
-            response.json().then(function(data) {
-                console.log(data);
-            });
-});
+var currentcityEl = document.querySelector(".current-city");
 
 var currentWeatherEl = document.querySelector(".current-weather");
+
+var getCityWeather = function(cityUrl) {
+
+    fetch(cityUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            console.log(data);
+            displayWeather(data);
+          });
+        }
+        else {
+          alert("There was a problem with your request!");
+        }
+    });
+}
+
+getCityWeather(cityUrl);
+
+var displayWeather = function(weather) {
+
+    currentcityEl.textContent = city;
+
+};
